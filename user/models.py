586e103d9ddc6b6ed189f9f21d871eb26934fd7a
surajit <surajit@poweredbypeople.io>
2021-02-24 15:36:33 +0300

    add absolute_url to user model

--- user/models.py
+++ user/models.py
@@ -2,8 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 
-
 # Create your models here.
+from django.urls import reverse
+
+
 class CustomUser(AbstractUser):
     # add additional fields in here
     profile_id = models.CharField(default=uuid.uuid4(), max_length=150)
@@ -14,3 +16,6 @@ class CustomUser(AbstractUser):
     def can_view(self, obj):
         if obj.profile_id == self.profile_id:
             return True
+
+    def get_absolute_url(self):
+        return reverse("user:profile_detail", args=[str(self.profile_id)])
