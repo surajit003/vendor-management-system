60d5747550c82ffab7cfdf55fd6bcf6d72dfd2d4
surajit <surajit@poweredbypeople.io>
2021-02-24 11:53:25 +0300

    modify root urls

--- VendorManagementSystem/urls.py
+++ VendorManagementSystem/urls.py
@@ -6,18 +6,13 @@ from django.conf.urls.static import static
 import debug_toolbar
 
 app_name = "vendor"
-main = [
+urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^__debug__/", include(debug_toolbar.urls)),
     url(r"^accounts/", include("allauth.urls")),
     url(r"^catalog/", include("catalog.urls")),
-]
-
-urlpatterns = (
-    [url(r"^vendor/", include(main))]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Vendor Admin"
 admin.site.site_title = "Vendor Admin Portal"
