e7772b06a38176986bf23481da5dffe7b9070ada
surajit <surajit@poweredbypeople.io>
2021-02-24 14:29:48 +0300

    add custom user form

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # add additional fields in here
+    profile_id = models.CharField(default=uuid.uuid4(), max_length=150)
+
+    def __str__(self):
+        return str(self.profile_id)
