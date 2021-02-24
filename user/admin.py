3ac6dd49f7ec65a473691bb62e2be2638e20981e
surajit <surajit@poweredbypeople.io>
2021-02-24 14:30:25 +0300

    add useradmin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
+
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('profile_id', 'first_name', 'last_name',)
--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 
 class SignUpForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = User
+    class Meta:
+        model = CustomUser
         fields = ('first_name', 'last_name','email','username', 'password1', 'password2',)
+
+    def clean(self):
+        data = self.cleaned_data
+        if "password1" in data and "password2" in data:
+            if data["password1"] != data["password2"]:
+                self._errors["password2"] = self.error_class(['Passwords do not match.'])
+                del data['password2']
+        return data
